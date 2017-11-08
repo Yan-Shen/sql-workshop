c80a0cbd6e2ac0272d9f7fa257d16030ce698273
Yan.Shen <sheny308@yahoo.com>
2017-11-08 15:48:29 -0500

    almost there

--- test.sql
+++ test.sql
@@ -21,3 +21,41 @@ where actors.first_name <> 'Kevin'
 and actors.last_name <> 'Bacon'
 limit 10;
 
+select actors.first_name, actors.last_name, actors.id
+from actors inner join
+roles on actors.id = roles.actor_id
+inner join movies
+on roles.movie_id = movies.id
+where movies.year < 1900
+INTERSECT
+select actors.first_name, actors.last_name, actors.id
+from actors inner join
+roles on actors.id = roles.actor_id
+inner join movies
+on roles.movie_id = movies.id
+where movies.year > 2000
+order by actors.id;
+
+select actors.first_name, actors.last_name, movies.name, roles.role, movies.year, COUNT (distinct roles.role) as count
+from actors inner join
+roles on actors.id = roles.actor_id
+inner join movies
+on roles.movie_id = movies.id
+where movies.year > 1990
+group by actors.id, movies.id
+having count >=5
+limit 10;
+
+select count (*), sub.year
+from
+(select movies.id, movies.year
+from actors inner join
+roles on actors.id = roles.actor_id
+inner join movies
+on roles.movie_id = movies.id
+where actors.gender <> 'M'
+and actors.gender <> null
+group by movies.id
+) sub
+group by sub.year
+limit 10;
